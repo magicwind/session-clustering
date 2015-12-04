556b1d082f619d0797169b0c441ebdeaffef9abf
Xu Feng <magicwind@msn.com>
2015-12-04 20:11:18 +0800

    implemented sim function

--- src/xu/feng/SessionSimilarity.java
+++ src/xu/feng/SessionSimilarity.java
@@ -6,7 +6,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class SessionSimilarity {
 
@@ -16,15 +19,20 @@ public class SessionSimilarity {
 		BufferedReader br = new BufferedReader(reader);
 		String line;
 		
-		Map<String, ArrayList<String>> sessionMap = new HashMap<String, ArrayList<String>>();
+		Map<String, List<String>> sessionMap = new HashMap<String, List<String>>();
+        Set<String> urlSet = new HashSet<String>();
+
 		while ((line = br.readLine()) != null) {
 			System.out.println(line);
 			String[] strArr = line.split(",");
 			String sessionId = strArr[0], url = strArr[1];
+            // add url to set
+            urlSet.add(url);
+
 			if (sessionMap.containsKey(sessionId)) {
 				sessionMap.get(sessionId).add(url);
 			} else {
-				ArrayList<String> newSession = new ArrayList<String>();
+				List<String> newSession = new ArrayList<String>();
 				if (newSession.add(url)) {
 					sessionMap.put(sessionId, newSession);
 				} else {
@@ -33,12 +41,32 @@ public class SessionSimilarity {
 			}
 		}
 
+        br.close();
+
 		System.out.println(sessionMap.toString());
 		
-		System.out.println("Sim1 = " + calSessionSim1(sessionMap.get("s1"), sessionMap.get("s2")));
+		double sim1 = calSessionSim1(sessionMap.get("s1"), sessionMap.get("s2"));
+		
+        System.out.println("Sim1 = " + sim1);
+
+        for (String url : sessionMap.get("s1")) {
+            System.out.println(url + " degree: " + getUrlDegree(url));
+        }
+
+        double sim2 = calSessionSim2(sessionMap.get("s1"), sessionMap.get("s2"));
+
+        System.out.println("Sim2 = " + sim2);
+
+        System.out.println("url sim: " + calUrlSim("/", "/info/prospective/"));
+        System.out.println("url sim: " + calUrlSim("/info/prospective/", "/"));
+        System.out.println("url sim: " + calUrlSim("/", "/"));
+        System.out.println("url sim: "
+                + calUrlSim("/education/computer-science/programinfo.html", "/education/computer-science/programapply.htm"));
+
+        System.out.println("final sim: " + Math.max(sim1, sim2));
 	}
 	
-	static double calSessionSim1(ArrayList<String> s1, ArrayList<String> s2) {
+    static double calSessionSim1(List<String> s1, List<String> s2) {
 		double sim1 = 0;
 		if (s1.size() == 0 || s2.size() == 0) {
 			return sim1;
@@ -57,7 +85,71 @@ public class SessionSimilarity {
 		return sim1;
 	}
 	
-	static double calSessionSim2(ArrayList<String> s1, ArrayList<String> s2) {
-		return 0;
+    static double calSessionSim2(List<String> s1, List<String> s2) {
+        double sim2 = 0;
+        double numerator = 0;
+        // double denominator = 1;
+        double urlSim = 0;
+        for (String s1url : s1) {
+            for (String s2url : s2) {
+                if (s1url.equals(s2url)) {
+                    numerator++;
+                } else {
+                    urlSim = calUrlSim(s1url, s2url);
+                    numerator += urlSim;
+                }
+            }
+        }
+
+        sim2 = numerator / (s1.size() * s2.size());
+
+        return sim2;
 	}
+
+    static int getUrlDegree(String url) {
+        int degree = 0;
+        int pos = -1;
+        while ((pos = url.indexOf("/")) != -1) {
+            url = url.substring(pos + 1);
+            if (pos == 0 || url.length() > 0) {
+                degree++;
+            } else {
+                break;
+            }
+        }
+
+        return degree;
+    }
+
+    static double calUrlSim(String url1, String url2) {
+        if (!url1.startsWith("/") || !url2.startsWith("/")) {
+            throw new RuntimeException("url does not start with /: " + url1 + " or " + url2);
+        }
+        int sameDegree = 0;
+        int degree = Math.min(getUrlDegree(url1), getUrlDegree(url2));
+
+        String[] tokenArr1 = url1.split("/");
+        String[] tokenArr2 = url2.split("/");
+
+        for (int i = 1; i <= degree; i++) {
+            String token1, token2;
+            if (tokenArr1.length == 0) {
+                token1 = "";
+            } else {
+                token1 = tokenArr1[i];
+            }
+
+            if (tokenArr2.length == 0) {
+                token2 = "";
+            } else {
+                token2 = tokenArr2[i];
+            }
+            
+            if (token1.equals(token2)) {
+                sameDegree++;
+            }
+        }
+
+        return sameDegree / (Math.sqrt(getUrlDegree(url1)) * Math.sqrt(getUrlDegree(url2)));
+    }
 }
